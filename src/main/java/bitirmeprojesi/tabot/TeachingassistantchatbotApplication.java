88536cf782a947439e1faa7701e7a647a07839a7
oykue <oykuergun07@gmail.com>
2022-04-05 19:48:04 +0300

    feat: add logging and encryption mechanism

--- src/main/java/bitirmeprojesi/tabot/TeachingassistantchatbotApplication.java
+++ src/main/java/bitirmeprojesi/tabot/TeachingassistantchatbotApplication.java
@@ -1,28 +1,32 @@
 package bitirmeprojesi.tabot;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Slf4j
 @SpringBootApplication
 @EnableSwagger2
 public class TeachingassistantchatbotApplication {
+
+	private static final Logger LOGGER= LoggerFactory.getLogger(TeachingassistantchatbotApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeachingassistantchatbotApplication.class, args);
 	}
 
 	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("bitirmeprojesi.tabot"))
-				.build();
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
