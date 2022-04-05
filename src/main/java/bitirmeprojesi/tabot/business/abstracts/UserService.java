3165cf1207638212f5d8b0e814597432a9e638b0
oykue <oykuergun07@gmail.com>
2022-04-05 19:46:19 +0300

    feat: add getUser method to UserService
    -Adding getUser method to user service for getting user by id.

--- src/main/java/bitirmeprojesi/tabot/business/abstracts/UserService.java
+++ src/main/java/bitirmeprojesi/tabot/business/abstracts/UserService.java
@@ -4,4 +4,5 @@ import bitirmeprojesi.tabot.entities.concretes.User;
 public interface UserService {
     void saveUser(User user);
     User login(String email, Integer password);
+    User getUser(int id);
 }
